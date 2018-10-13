96367edbcf2c3c26bbbc19339e095491eb18460a
Ted <bbielo20@gmail.com>
2018-10-13 15:01:39 +0900

    9장 프로젝트: Menu 컴포넌트_ class기반 컴포넌트를 함수형으로 refactoring

--- ch09/menu-jsx/js/script.js
+++ ch09/menu-jsx/js/script.js
@@ -1,3 +1,5 @@
+/*
+//class 형식 _ step.1
 class Link extends React.Component {
     constructor(props) {
         super(props);
@@ -5,33 +7,67 @@ class Link extends React.Component {
 
     render() {
         const url = "/" + this.props.label.toLowerCase().trim().replace(" ", "-");
-        return React.createElement(
-            "div",
-            null,
-            React.createElement(
-                "a",
-                { href: url },
-                this.props.label
-            )
+        return (
+            <div>
+                <a href={url}>{this.props.label}</a>
+            </div>
         );
     }
 }
 
 class Menu extends React.Component {
     render() {
-        let menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us'];
-        return React.createElement(
-            "div",
-            null,
-            menus.map((value, index) => {
-                return React.createElement(
-                    "div",
-                    { key: index },
-                    React.createElement(Link, { label: value })
-                );
-            })
+        let menus = [
+            'Home',
+            'About',
+            'Services',
+            'Portfolio',
+            'Contact us'
+        ];
+        return (
+            <div>
+                {menus.map((value, index) => {
+                    return (
+                        <div key={index}>
+                            <Link label={value}/>
+                        </div>
+                    )
+                })}
+            </div>
         );
     }
 }
+*/
+
+// function 형식 _ refactoring (컴포넌트에 this.state와 lifeCycle이 없으면 함수형으로 refactoring 이 가능
+const Link = props => {
+    let url = "/" + props.label.toLowerCase().trim().replace(" ", "-");
+
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "a",
+            { href: url },
+            props.label
+        )
+    );
+};
+
+const Menu = () => {
+    let menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us'];
+
+    return React.createElement(
+        "div",
+        null,
+        menus.map((value, index) => {
+            return React.createElement(
+                "div",
+                { key: index },
+                React.createElement(Link, { label: value })
+            );
+        })
+    );
+};
 
 ReactDOM.render(React.createElement(Menu, null), document.getElementById('menu'));
--- ch09/menu-jsx/jsx/script.jsx
+++ ch09/menu-jsx/jsx/script.jsx
@@ -1,3 +1,5 @@
+/*
+//class 형식 _ step.1
 class Link extends React.Component {
     constructor(props) {
         super(props);
@@ -35,6 +37,41 @@ class Menu extends React.Component {
         );
     }
 }
+*/
+
+// function 형식 _ refactoring (컴포넌트에 this.state와 lifeCycle이 없으면 함수형으로 refactoring 이 가능
+const Link = (props) => {
+    let url = "/" + props.label.toLowerCase().trim().replace(" ", "-");
+
+    return (
+        <div>
+            <a href={url}>{props.label}</a>
+        </div>
+    );
+};
+
+const Menu =() => {
+    let menus = [
+            'Home',
+            'About',
+            'Services',
+            'Portfolio',
+            'Contact us'
+    ];
+
+    return (
+        <div>
+            {menus.map((value, index) => {
+                            return (
+                                <div key={index}>
+                                    <Link label={value}/>
+                                </div>
+                            );
+                        })
+            }
+        </div>
+    );
+};
 
 ReactDOM.render(
     <Menu/>,
